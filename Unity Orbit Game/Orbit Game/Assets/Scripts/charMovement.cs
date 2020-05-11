54047f0f12050e181c91ece740e925aee57e1386
Particleman376 <44531799+Particleman376@users.noreply.github.com>
2020-05-11 10:38:06 -0500

    Added world bounds

--- Unity Orbit Game/Orbit Game/Assets/Scripts/charMovement.cs
+++ Unity Orbit Game/Orbit Game/Assets/Scripts/charMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class charMovement : MonoBehaviour
 {
     float timeSinceTouch;
-    int speed = 5;
+    int speed = 7;
     public Transform targetPos;
     Vector3 goalPos;
     // Start is called before the first frame update
@@ -20,7 +20,7 @@ public class charMovement : MonoBehaviour
         goalPos = targetPos.position - transform.position;
         if(timeSinceTouch > 1)
         {
-            transform.Translate(Time.deltaTime * goalPos, Space.World);
+            transform.Translate(speed * Time.deltaTime * goalPos, Space.World);
         }
 
         transform.Translate(Input.GetAxis("Horizontal") * speed  * Time.deltaTime,Input.GetAxis("Vertical") * speed * Time.deltaTime, 0, Space.World);
@@ -29,7 +29,7 @@ public class charMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(Input.GetAxis("Horizontal") == 0.0 || Input.GetAxis("Vertical") == 0.0)
+        if(Input.GetAxis("Horizontal") == 0.0 & Input.GetAxis("Vertical") == 0.0)
         {
             timeSinceTouch += Time.fixedDeltaTime;
         }
