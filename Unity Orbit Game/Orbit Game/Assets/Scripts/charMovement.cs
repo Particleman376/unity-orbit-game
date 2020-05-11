81d175970241057d8ae3ec7918b04fecb5af25c1
Particleman376 <44531799+Particleman376@users.noreply.github.com>
2020-05-10 21:07:47 -0500

    Added snap back to center with movoment inactivity

--- Unity Orbit Game/Orbit Game/Assets/Scripts/charMovement.cs
+++ Unity Orbit Game/Orbit Game/Assets/Scripts/charMovement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class charMovement : MonoBehaviour
 {
+    float timeSinceTouch;
     int speed = 5;
+    public Transform targetPos;
+    Vector3 goalPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,26 @@ public class charMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        goalPos = targetPos.position - transform.position;
+        if(timeSinceTouch > 1)
+        {
+            transform.Translate(Time.deltaTime * goalPos, Space.World);
+        }
+
         transform.Translate(Input.GetAxis("Horizontal") * speed  * Time.deltaTime,Input.GetAxis("Vertical") * speed * Time.deltaTime, 0, Space.World);
+
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetAxis("Horizontal") == 0.0 || Input.GetAxis("Vertical") == 0.0)
+        {
+            timeSinceTouch += Time.fixedDeltaTime;
+        }
+        else
+        {
+            timeSinceTouch = 0;
+        }
     }
 }
