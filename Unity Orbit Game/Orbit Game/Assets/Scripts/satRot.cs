a35f300c5ba81e1acae407727be39e09006ff02e
Particleman376 <44531799+Particleman376@users.noreply.github.com>
2020-05-13 22:44:33 -0500

    Update satRot.cs

--- Unity Orbit Game/Orbit Game/Assets/Scripts/satRot.cs
+++ Unity Orbit Game/Orbit Game/Assets/Scripts/satRot.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 
 public class satRot : MonoBehaviour
 {
-    GameObject[] satellites;
     int arrNumb = 0;
     public int satNumb = 0;
     // Start is called before the first frame update
     void Awake()
     {
-        satellites = GameObject.FindGameObjectsWithTag("Electron");
     }
 
     // Update is called once per frame
     void Update()
     {
-       foreach(GameObject satellite in satellites)
-       {
-           satNumb++;
-           print(satNumb);
-       }
+
     }
 }
