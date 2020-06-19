df0b09334ab42ab25bb9d3a4fdf3048da002535d
Particleman376 <44531799+Particleman376@users.noreply.github.com>
2020-06-19 12:48:16 -0500

    Added 'shells' of electrons, made the count in each shell accurate to real life, but can't go higher than 3 levels because of float roundning

--- Unity Orbit Game/Orbit Game/Assets/Scripts/satRot.cs
+++ Unity Orbit Game/Orbit Game/Assets/Scripts/satRot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class satRot : MonoBehaviour
 {
+    int offSet = 0;
+    int mySet = 0;
     public int arrNumb = 0;
     public int satNumb = 0;
     int satsDown = 0;
@@ -17,7 +19,20 @@ public class satRot : MonoBehaviour
     void Awake()
     {
         cloneScript = GameObject.Find("Player").GetComponent<satSpawner>();
-        arrNumb = cloneScript.satCount - 1;
+        mySet = cloneScript.set;
+        switch(cloneScript.set)
+        {
+            case 1:
+                arrNumb = cloneScript.satCount1 - 1;
+                break;
+            case 2:
+                arrNumb = cloneScript.satCount2 - 1;
+                break;
+            case 3:
+                arrNumb = cloneScript.satCount3 - 1;
+                break;
+        }
+        
         //print(arrNumb);
     }
 
@@ -25,20 +40,47 @@ public class satRot : MonoBehaviour
     void LateUpdate()
     {
         cloneScript = GameObject.Find("Player").GetComponent<satSpawner>();
-        satNumb = cloneScript.satCount;
+        switch(cloneScript.set)
+        {
+            case 1:
+                satNumb = cloneScript.satCount1;
+                offSet = 0;
+                break;
+            case 2:
+                satNumb = cloneScript.satCount2;
+                offSet = 15;
+                break;
+            case 3:
+                satNumb = cloneScript.satCount3;
+                offSet = 30;
+                break; 
+        }
         steps = 360/satNumb;
-        goalRot = Quaternion.Euler(0, 0, steps * arrNumb); //something is wrong here I think
-        
+        goalRot = Quaternion.Euler(0, 0, steps * arrNumb + offSet);
 
-
-        if(Input.GetButtonDown("SpawnSat"))
+        if(Input.GetButtonDown("SpawnSat") & mySet == cloneScript.set)
         {
             if(arrNumb == (satsDown - 1))
             {
                 print(goalRot.eulerAngles - transform.eulerAngles);
             } 
-            transform.Rotate(goalRot.eulerAngles - transform.eulerAngles);   
+        transform.Rotate(goalRot.eulerAngles - transform.eulerAngles);   
         }
+
+        if(mySet == 1)
+        {
+            transform.Rotate(0, 0, Time.deltaTime * 350);
+        }
+        else if(mySet == 2)
+        {
+            transform.Rotate(0, 0, Time.deltaTime * -1);
+        }
+        else if(mySet == 3)
+        {
+            transform.Rotate(0, 0,Time.deltaTime * -100);
+        }
+
+        
             
 
     }
--- Unity Orbit Game/Orbit Game/Assets/Scripts/satSpawner.cs
+++ Unity Orbit Game/Orbit Game/Assets/Scripts/satSpawner.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class satSpawner : MonoBehaviour
 {
+    public int set = 0;
     public Transform parent;
-    public GameObject satChild;
-    float satAngle = 0.000f;
-    public int satCount = 0;
+    public GameObject satChild1;
+    public GameObject satChild2;
+    public GameObject satChild3;
+    double satAngle = 0.000f;
+    public int satCount1 = 0;
+    public int satCount2 = 0;
+    public int satCount3 = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,13 +26,40 @@ public class satSpawner : MonoBehaviour
 
         if(Input.GetButtonDown("SpawnSat"))
         {     
-            satCount ++;
-            satAngle = 360 / satCount;
-            Quaternion offSet = Quaternion.Euler(0, 0, satAngle * (satCount - 1));
-            //print(offSet.eulerAngles);
-            Quaternion actAngle = Quaternion.Euler(parent.rotation.eulerAngles + offSet.eulerAngles); 
-            GameObject sat;
-            sat = Instantiate(satChild, parent.position, actAngle, parent);
+            if(satCount1 < 2)
+            {
+                set = 1;
+                satCount1 ++;
+                satAngle = 360 / satCount1;
+                Quaternion offSet = Quaternion.Euler(0, 0, (float)(satAngle * (satCount1 - 1)));
+                //print(offSet.eulerAngles);
+                Quaternion actAngle = Quaternion.Euler(parent.rotation.eulerAngles + offSet.eulerAngles); 
+                GameObject sat;
+                sat = Instantiate(satChild1, parent.position, actAngle, parent);
+            }
+            else if(satCount2 < 8)
+            {
+                set = 2;
+                satCount2 ++;
+                satAngle = 360 / satCount2;
+                Quaternion offSet = Quaternion.Euler(0, 0, (float)(satAngle * (satCount2 - 1)));
+                //print(offSet.eulerAngles);
+                Quaternion actAngle = Quaternion.Euler(parent.rotation.eulerAngles + offSet.eulerAngles); 
+                GameObject sat;
+                sat = Instantiate(satChild2, parent.position, actAngle, parent);
+            }
+            else if(satCount3 < 18)
+            { 
+                set = 3;
+                satCount3 ++;
+                satAngle = 360 / satCount3;
+                Quaternion offSet = Quaternion.Euler(0, 0, (float)(satAngle * (satCount3 - 1)));
+                //print(offSet.eulerAngles);
+                Quaternion actAngle = Quaternion.Euler(parent.rotation.eulerAngles + offSet.eulerAngles); 
+                GameObject sat;
+                sat = Instantiate(satChild3, parent.position, actAngle, parent);
+            }
+
         }
 
     }
